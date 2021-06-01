212b10da7ea88217b8bd31f0c7ccb1a39a947262
19044628 <19044628@rp.edu.sg>
2021-06-01 11:50:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19044628/demooptionmenutranslation/MainActivity.java
@@ -0,0 +1,45 @@
+package sg.edu.rp.c346.id19044628.demooptionmenutranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTranslatedtext, tvTranslatedtext2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedtext.setText("Hello ");
+            return true;
+        } else if (id == R.id.italianSelection) {
+            tvTranslatedtext.setText("Ciao");
+            return true;
+        } else {
+            tvTranslatedtext.setText("Error translation");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
